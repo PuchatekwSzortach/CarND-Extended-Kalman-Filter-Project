55313a0746404f5f7851dc558173a4e429a31b0f
Kolodziejczyk Jakub <k.jakub@lexues.co.jp>
2017-05-23 12:17:48 +0900

    Set estimates to be equal to lasar measurments

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -12,22 +12,23 @@ using std::vector;
  * Constructor.
  */
 FusionEKF::FusionEKF() {
-  is_initialized_ = false;
 
-  previous_timestamp_ = 0;
+  this->is_initialized_ = false;
+
+  this->previous_timestamp_ = 0;
 
   // initializing matrices
-  R_laser_ = MatrixXd(2, 2);
-  R_radar_ = MatrixXd(3, 3);
-  H_laser_ = MatrixXd(2, 4);
-  Hj_ = MatrixXd(3, 4);
+  this->R_laser_ = MatrixXd(2, 2);
+  this->R_radar_ = MatrixXd(3, 3);
+  this->H_laser_ = MatrixXd(2, 4);
+  this->Hj_ = MatrixXd(3, 4);
 
   //measurement covariance matrix - laser
-  R_laser_ << 0.0225, 0,
+  this->R_laser_ << 0.0225, 0,
         0, 0.0225;
 
   //measurement covariance matrix - radar
-  R_radar_ << 0.09, 0, 0,
+  this->R_radar_ << 0.09, 0, 0,
         0, 0.0009, 0,
         0, 0, 0.09;
 
@@ -36,6 +37,7 @@ FusionEKF::FusionEKF() {
     * Finish initializing the FusionEKF.
     * Set the process and measurement noises
   */
+
 
 
 }
@@ -47,7 +49,6 @@ FusionEKF::~FusionEKF() {}
 
 void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
-
   /*****************************************************************************
    *  Initialization
    ****************************************************************************/
@@ -58,24 +59,42 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
       * Create the covariance matrix.
       * Remember: you'll need to convert radar from polar to cartesian coordinates.
     */
+
+    double px = 0 ;
+    double py = 0 ;
+    double vx = 0 ;
+    double vy = 0 ;
+
+    if(measurement_pack.sensor_type_ == MeasurementPackage::LASER)
+    {
+      px = measurement_pack.raw_measurements_[0] ;
+      py = measurement_pack.raw_measurements_[1] ;
+    }
+
+    if(measurement_pack.sensor_type_ == MeasurementPackage::RADAR)
+    {
+      std::cout << "We got ourselves a RADAR reading" << std::endl;
+      std::cout << "Not handling that yet" << std::endl ;
+    }
+
     // first measurement
     cout << "EKF: " << endl;
     ekf_.x_ = VectorXd(4);
-    ekf_.x_ << 1, 1, 1, 1;
-
-    if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
-      /**
-      Convert radar from polar to cartesian coordinates and initialize state.
-      */
-    }
-    else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
-      /**
-      Initialize state.
-      */
-    }
-
-    // done initializing, no need to predict or update
-    is_initialized_ = true;
+    ekf_.x_ << px, py, vx, vy ;
+//
+//    if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
+//      /**
+//      Convert radar from polar to cartesian coordinates and initialize state.
+//      */
+//    }
+//    else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
+//      /**
+//      Initialize state.
+//      */
+//    }
+//
+//    // done initializing, no need to predict or update
+//    is_initialized_ = true;
     return;
   }
 
--- src/main.cpp
+++ src/main.cpp
@@ -110,42 +110,28 @@ int main()
           ground_truth.push_back(gt_values);
 
           //Call ProcessMeasurment(meas_package) for Kalman filter
-//          fusionEKF.ProcessMeasurement(meas_package);
+          fusionEKF.ProcessMeasurement(meas_package);
 
           //Push the current estimated x,y positon from the Klaman filter's state vector
-
           VectorXd estimate(4);
 
-//          double p_x = fusionEKF.ekf_.x_(0);
-//          double p_y = fusionEKF.ekf_.x_(1);
-//          double v1  = fusionEKF.ekf_.x_(2);
-//          double v2 = fusionEKF.ekf_.x_(3);
-//
-//          estimate(0) = p_x;
-//          estimate(1) = p_y;
-//          estimate(2) = v1;
-//          estimate(3) = v2;
+          double p_x = fusionEKF.ekf_.x_(0);
+          double p_y = fusionEKF.ekf_.x_(1);
+          double v1  = fusionEKF.ekf_.x_(2);
+          double v2 = fusionEKF.ekf_.x_(3);
 
-          estimate(0) = x_gt + 5;
-          estimate(1) = y_gt + 5;
-          estimate(2) = vx_gt + 10;
-          estimate(3) = vy_gt + 10;
+          estimate(0) = p_x;
+          estimate(1) = p_y;
+          estimate(2) = v1;
+          estimate(3) = v2;
 
           estimations.push_back(estimate);
 
           VectorXd RMSE = tools.CalculateRMSE(estimations, ground_truth);
 
-//          json msgJson;
-//          msgJson["estimate_x"] = p_x;
-//          msgJson["estimate_y"] = p_y;
-//          msgJson["rmse_x"] =  RMSE(0);
-//          msgJson["rmse_y"] =  RMSE(1);
-//          msgJson["rmse_vx"] = RMSE(2);
-//          msgJson["rmse_vy"] = RMSE(3);
-
           json msgJson;
-          msgJson["estimate_x"] = estimate(0);
-          msgJson["estimate_y"] = estimate(1);
+          msgJson["estimate_x"] = p_x;
+          msgJson["estimate_y"] = p_y;
           msgJson["rmse_x"] =  RMSE(0);
           msgJson["rmse_y"] =  RMSE(1);
           msgJson["rmse_vx"] = RMSE(2);
