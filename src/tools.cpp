e898e1410726006197884af629ebdf304c1e60e6
Kolodziejczyk Jakub <k.jakub@lexues.co.jp>
2017-05-23 11:56:52 +0900

    Faked estimate values to be at a constant offset from groundtruth as a placeholder. Added Tools::CalculateRMSE() implementation

--- src/tools.cpp
+++ src/tools.cpp
@@ -11,10 +11,21 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+  VectorXd errors(4) ;
+  errors << 0, 0, 0, 0 ;
+
+  // Sum square sums of errors between ground truth and observations so far
+  for(int i = 0 ; i < estimations.size() ; ++i)
+  {
+
+    VectorXd difference = (estimations[i] - ground_truth[i]) ;
+    VectorXd squared_difference = difference.array() * difference.array() ;
+
+    errors += squared_difference ;
+  }
+
+  // Now divide by number of samples and take sqrt for a complete RMSE
+  return (errors / estimations.size()).array().sqrt() ;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
